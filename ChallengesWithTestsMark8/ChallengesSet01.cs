ae8e5c26f069dbd8651c356e668780209f0e6e0c
Adam Measel <ameasel55@gmail.com>
2023-02-09 10:08:42 -0600

    1st challenge

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -3,8 +3,10 @@
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet01
+
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
+            
         {
             if(num1 == num2)
             {
@@ -17,33 +19,51 @@ namespace ChallengesWithTestsMark8
         }
 
         public double Subtract(double minuend, double subtrahend)
+
         {
-            throw new NotImplementedException();
+           double answer = minuend - subtrahend;
+           
+                return answer;
+            
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            int answer = number1 + number2;
+
+            return answer;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 > number2)
+            {
+                return number2;
+            }
+            else
+            {
+                return number1;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            long answer = factor1 * factor2;
+                return answer;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            else return "Hello, " + nameOfPerson + "!";
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
